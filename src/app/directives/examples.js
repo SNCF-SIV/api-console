9f29aa08f5b7cbc8e35c55e7e90e6847c7764852
Nicolas Payneau <nicolas.payneau@gmail.com>
2016-10-06 14:33:26 +0200

    Add "Show example" button like "Show Schema"

--- src/app/directives/examples.js
+++ src/app/directives/examples.js
@@ -20,6 +20,26 @@
           $scope.currentExample = example;
         };
 
+       $scope.showExample = function ($event) {
+                      var $this   = jQuery($event.currentTarget);
+                      var $panel  = $this.closest('.raml-console-examples-container');
+                      var $example = $panel.find('.raml-console-resource-pre-toggle');
+
+                      $this.toggleClass('raml-console-is-active');
+
+                      if (!$example.hasClass('raml-console-is-active')) {
+                        $this.text('Hide Example');
+                        $example
+                          .addClass('raml-console-is-active')
+                          .velocity('slideDown');
+                      } else {
+                        $this.text('Show Example');
+                        $example
+                          .removeClass('raml-console-is-active')
+                          .velocity('slideUp');
+                      }
+                    };
+
         $scope.$watch('exampleContainer', function (value) {
           $scope.examples = transformExample(value);
         });
